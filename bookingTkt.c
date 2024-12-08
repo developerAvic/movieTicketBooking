81364142b9d0267ebc2f9d7842c2891da50d4590
developerAvic <ashirvadbless@gmail.com>
2024-12-08 09:35:28 +0530

    declared structures

--- bookingTkt.c
+++ bookingTkt.c
@@ -1,6 +1,27 @@
 
 
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h> //required for system()
-#include<ctype.h>
+
+
+//Structure for movie details
+struct movie
+{
+    char title[50];   /* data */
+    char genre[20];
+    int duration;
+    float tkt_Price;
+};
+
+// structure for snacks and beverages
+struct snacks
+{
+    char name[50];
+    float snk_price;
+};
+
+int main()
+{
+printf("Welcome to Movie Booking System.\n");
+
+return 0;
+}
